743aeff9a7b4b28e00485931a8c9b504b7710d37
Caroline Taymor <ctaymor@gmail.com>
2025-06-10 14:43:22 -0700

    Generate static resources for deployment for clojure site

--- clojure-tech-site/src/consulting_site/core.clj
+++ clojure-tech-site/src/consulting_site/core.clj
@@ -1,6 +1,7 @@
 ; src/consulting_site/core.clj - Main application file
 (ns consulting-site.core
   (:require [ring.adapter.jetty :as jetty]
+            [clojure.java.io :as io]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
@@ -39,7 +40,53 @@
       (wrap-defaults site-defaults)
       (wrap-content-type)))
 
-;; Server entry point
-(defn -main []
-  (jetty/run-jetty app {:port 3000 :join? false})
-  (println "Server started on port 3000"))
+(defn copy-resources []
+  "Copy static resources from resources/public to dist"
+  (let [source-dir "resources/public/"
+        dest-dir "dist/"]
+    (when (.exists (java.io.File. source-dir))
+      ;; Copy all files from resources/public to dist
+      (doseq [file (file-seq (io/file source-dir))]
+        (when (.isFile file)
+          (let [relative-path (.substring (.getPath file) (count source-dir))
+                dest-file (io/file dest-dir relative-path)]
+            (io/make-parents dest-file)
+            (io/copy file dest-file)))))))
+
+(defn generate-static-site []
+  (let [output-dir "dist/"]
+    (.mkdirs (java.io.File. output-dir))
+
+    ;; Create directories for each route
+    (.mkdirs (java.io.File. (str output-dir "about/")))
+    (.mkdirs (java.io.File. (str output-dir "services/")))
+    (.mkdirs (java.io.File. (str output-dir "contact/")))
+    (.mkdirs (java.io.File. (str output-dir "blog/")))
+
+    ;; Generate as index.html in each directory
+    (spit (str output-dir "index.html") (layout/render-page (home/page)))
+    (spit (str output-dir "about/index.html") (layout/render-page (about/page)))
+    (spit (str output-dir "services/index.html") (layout/render-page (services/page)))
+    (spit (str output-dir "contact/index.html") (layout/render-page (contact/page)))
+
+    ;; Blog
+    (let [posts (blog/load-local-posts)]
+      (spit (str output-dir "blog/index.html") (layout/render-page (blog-views/page posts)))
+
+      ;; Individual posts
+      (doseq [post posts]
+        (let [post-dir (str output-dir "blog/" (:id post) "/")]
+          (.mkdirs (java.io.File. post-dir))
+          (spit (str post-dir "index.html") (layout/render-page (blog-views/post post))))))
+
+    (copy-resources)
+    (println "Static site generated in" output-dir)))
+
+;; Entry point
+(defn -main [& args]
+  (if (= (first args) "build")
+    (generate-static-site)
+    (do
+      (jetty/run-jetty app {:port 3000 :join? false})
+      (println "Server started on port 3000"))))
+
