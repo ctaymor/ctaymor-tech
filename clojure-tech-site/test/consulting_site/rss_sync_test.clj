da627720bb95fc7786c83abb1dde768f103b7a97
Caroline Taymor <ctaymor@gmail.com>
2025-06-16 12:08:45 -0700

    Fix tests for moving rss function to separate namespace

--- clojure-tech-site/test/consulting_site/rss_sync_test.clj
+++ clojure-tech-site/test/consulting_site/rss_sync_test.clj
@@ -1,19 +1,19 @@
-(ns consulting-site.blog-test
+(ns consulting-site.rss-sync-test
   (:require [clojure.test :refer :all]
-            [consulting-site.blog :as blog]
+            [consulting-site.rss-sync :as rss]
             [clojure.string :as str]))
 
 (deftest test-extract-posts-from-xml
   (testing "handles nil xml gracefully"
-    (is (nil? (blog/extract-posts-from-xml nil))))
+    (is (nil? (rss/extract-posts-from-xml nil))))
 
   #_(testing "handles empty xml"
-    (is (empty? (blog/extract-posts-from-xml {})))))
+    (is (empty? (rss/extract-posts-from-xml {}))))
 (deftest test-create-safe-excerpt
   (testing "handles nil description"
-    (is (= "No excerpt available" (blog/create-safe-excerpt nil 100))))
+    (is (= "No excerpt available" (rss/create-safe-excerpt nil 100))))
 
   (testing "creates excerpt from content"
     (is (str/starts-with?
-         (blog/create-safe-excerpt "This is a long description that should be truncated" 10)
+         (rss/create-safe-excerpt "This is a long description that should be truncated" 10)
          "This is a "))))
